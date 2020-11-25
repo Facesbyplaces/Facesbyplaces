438a000c9d32a85f9739184368a1ca6db8d3c266
Kimson Wong <kimsonwong@gmail.com>
2020-11-25 17:54:49 +0800

    Stagin stripe credentials update

--- app/controllers/api/v1/users/image_upload_controller.rb
+++ app/controllers/api/v1/users/image_upload_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::Users::ImageUploadController < ApplicationController
 
-    before_action :authenticate_user!
-
     def image_upload_params
         params.permit(:user_id, :image)
     end
