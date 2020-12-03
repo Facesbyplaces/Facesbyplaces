bff5375e1e0e4f5ddda9e0e3c92c6d26d6838afd
Kimson Wong <kimsonwong@gmail.com>
2020-12-03 10:21:20 +0800

    Added users data to image show

--- app/controllers/api/v1/users/image_show_controller.rb
+++ app/controllers/api/v1/users/image_show_controller.rb
@@ -7,7 +7,14 @@ class Api::V1::Users::ImageShowController < ApplicationController
 
     def index
         @user = User.find(params[:user_id])
-        puts @user.image
-        render json: {success: true, user: (request.base_url+url_for(@user.image)), status: 200}, status: 200      
+        render json: {
+            success: true, 
+            user: {
+                first_name: user.first_name.to_s,
+                last_name: user.last_name.to_s,
+                image: user.image.present? ? (request.base_url+url_for(user.image)) : "",
+            }, 
+            status: 200
+        }, status: 200      
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,6 @@ class User < ActiveRecord::Base
       user.email = provider_data.info.email
       user.password = Devise.friendly_token(0, 20)
     end
-
   end
 
   def self.new_guest
