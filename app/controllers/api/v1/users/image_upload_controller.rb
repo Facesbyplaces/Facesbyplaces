0bbd073c992545c6d18d0e56ade993e779795ab8
Kimson Wong <kimsonwong@gmail.com>
2020-12-03 16:04:02 +0800

    Image Upload bug fixes

--- app/controllers/api/v1/users/image_upload_controller.rb
+++ app/controllers/api/v1/users/image_upload_controller.rb
@@ -7,9 +7,11 @@ class Api::V1::Users::ImageUploadController < ApplicationController
     def update
         @user = User.find(params[:id])
 
-        if image_upload_params[:id].to_i === @user.id
+        if image_upload_params[:id].to_i === @user.id && @user.id === user.id
             @user.update(image: image_upload_params[:image])
-            render json: {success: true, message: "Successfully Uploaded Image", status: 200}, status: 200 
+            render json: {success: true, message: "Successfully Uploaded Image", status: 200}, status: 200
+        elsif  @user.id != user.id
+            render json: {success: false, message: "User with that ID not be found", status: 404}, status: 404
         elsif @user.errors.present?
             render json: {success: false, errors: @user.errors.full_messages, status: 404}, status: 200
         end
