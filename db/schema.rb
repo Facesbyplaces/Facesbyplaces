94d4bca373711a45ea36306bf3ba02736a56ed36
Kimson Wong <kimsonwong@gmail.com>
2020-12-04 12:10:54 +0800

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2020_12_03_162947) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "authorizations", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "blms", force: :cascade do |t|
     t.string "location"
     t.string "precinct"
@@ -94,9 +102,9 @@ ActiveRecord::Schema.define(version: 2020_12_03_162947) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "privacy"
-    t.string "stripe_connect_account_id"
     t.float "longitude"
     t.float "latitude"
+    t.string "stripe_connect_account_id"
     t.boolean "hideFamily"
     t.boolean "hideFriends"
     t.boolean "hideFollowers"
