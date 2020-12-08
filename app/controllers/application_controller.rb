6586d5fb3cb88006d12cd2b222e48a27e10e188b
paul <paul.marcuelo@kodakollectiv.com>
2020-12-08 19:30:20 +0800

    Current_user updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -100,6 +100,6 @@ class ApplicationController < ActionController::Base
         end
         
         def set_current_user
-            User.current = current_user 
+            current_user != nil ? User.current = current_user : ""
         end
 end
