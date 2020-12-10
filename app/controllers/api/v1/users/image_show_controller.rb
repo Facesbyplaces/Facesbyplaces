16e72efa20f832b0d59fef51ca62b4c9561df042
paul <paul.marcuelo@kodakollectiv.com>
2020-12-10 10:33:03 +0800

    image show added id

--- app/controllers/api/v1/users/image_show_controller.rb
+++ app/controllers/api/v1/users/image_show_controller.rb
@@ -5,6 +5,7 @@ class Api::V1::Users::ImageShowController < ApplicationController
         render json: {
             success: true, 
             user: {
+                id: user.id,
                 first_name: user.first_name.to_s,
                 last_name: user.last_name.to_s,
                 image: user.image.attached? ? (url_for(user.image)) : "",
