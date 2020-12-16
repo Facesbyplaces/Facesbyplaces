b794e48e251eac8c87f1482bddff020c70f5ba18
Kimson Wong <kimsonwong@gmail.com>
2020-12-16 12:23:43 +0800

    Users table update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_030204) do
+ActiveRecord::Schema.define(version: 2020_12_16_042203) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -280,8 +280,6 @@ ActiveRecord::Schema.define(version: 2020_12_16_030204) do
     t.boolean "hideEmail"
     t.boolean "hideAddress"
     t.boolean "hidePhonenumber"
-    t.string "facebook_id"
-    t.string "google_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
