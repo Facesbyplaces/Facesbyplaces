9220d06b1986f53dd74c6f022866bf960176f201
paul <paul.marcuelo@kodakollectiv.com>
2020-12-23 14:07:44 +0800

    session updates

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -61,11 +61,11 @@ class Api::V1::Users::SessionsController < DeviseTokenAuth::SessionsController
             @user.hidePhonenumber = false 
             @user.is_verified = true
 
-            # image
-            if params[:image].present? 
-              downloaded_image = open(params[:image])
-              @user.image.attach(io: downloaded_image  , filename: "foo.jpg")
-            end
+            # # image
+            # if params[:image].present? 
+            #   downloaded_image = open(params[:image])
+            #   @user.image.attach(io: downloaded_image  , filename: "foo.jpg")
+            # end
             @user.save!
     
             render json: UserSerializer.new( @user ).attributes
