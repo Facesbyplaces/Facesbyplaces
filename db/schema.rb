a382dca95d9ebfbd2e53f41c7b7fbec657685880
paul <paul.marcuelo@kodakollectiv.com>
2021-01-08 11:06:22 +0800

    added blm user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_014243) do
+ActiveRecord::Schema.define(version: 2021_01_08_030506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,31 @@ ActiveRecord::Schema.define(version: 2021_01_07_014243) do
     t.index ["email"], name: "index_alm_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_alm_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_alm_users_on_uid_and_provider", unique: true
+  end
+
+  create_table "blm_users", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.string "nickname"
+    t.string "image"
+    t.string "email"
+    t.json "tokens"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_blm_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_blm_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_blm_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_blm_users_on_uid_and_provider", unique: true
   end
 
   create_table "blms", force: :cascade do |t|
@@ -265,7 +290,6 @@ ActiveRecord::Schema.define(version: 2021_01_07_014243) do
   end
 
   create_table "shares", force: :cascade do |t|
-    t.integer "user_id"
     t.integer "content_type"
     t.integer "content_type_id"
     t.datetime "created_at", precision: 6, null: false
