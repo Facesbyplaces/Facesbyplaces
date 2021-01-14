145d77f292f5364ca9db3cc9d8fa948175fcead7
paul <paul.marcuelo@kodakollectiv.com>
2021-01-14 12:08:23 +0800

    iamge upload updates

--- app/controllers/api/v1/users/image_upload_controller.rb
+++ app/controllers/api/v1/users/image_upload_controller.rb
@@ -7,12 +7,16 @@ class Api::V1::Users::ImageUploadController < ApplicationController
 
     def update  
         user = user()
-        user.update(image: params[:image])
+        if user != nil
+            user.update(image: params[:image])
 
-        if user.errors.present?
-            render json: {success: false, errors: @image.errors.full_messages, status: 404}, status: 200
+            if user.errors.present?
+                render json: {success: false, errors: @image.errors.full_messages, status: 404}, status: 200
+            else
+                render json: {success: true, message: "Successfully Uploaded Image", status: 200}, status: 200
+            end
         else
-            render json: {success: true, message: "Successfully Uploaded Image", status: 200}, status: 200
+            render json: {error: "pls login"}, status: 422
         end
     end
 
