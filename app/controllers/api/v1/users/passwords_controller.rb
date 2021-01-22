9c76137b87cf397107b32c930541b3096f8bbb02
Kimson Wong <kimsonwong@gmail.com>
2021-01-22 11:50:02 +0800

    ALM Forgot Password fixed

--- app/controllers/api/v1/users/passwords_controller.rb
+++ app/controllers/api/v1/users/passwords_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::Users::PasswordsController < DeviseTokenAuth::PasswordsController
+
+    protected
+
     def render_update_success
-        render json: {
-        success: true,
-        data: resource_data,
-        message: I18n.t('devise_token_auth.passwords.successfully_updated')
-        }
+      render json: {
+       data: resource_data(resource_json: @resource.token_validation_response),
+       message: I18n.t('devise_token_auth.passwords.successfully_updated')
+      }, scope: :current_master
     end
 
 end
