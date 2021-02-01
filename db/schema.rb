1a2af0fce2659e59b923fc3747ee5c4b1dc38d46
Kimson Wong <kimsonwong@gmail.com>
2021-02-01 16:05:55 +0800

    Db updated

--- db/schema.rb
+++ db/schema.rb
@@ -210,8 +210,10 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.bigint "account_id"
     t.string "ignore_type", null: false
     t.bigint "ignore_id", null: false
+    t.bigint "user_id", null: false
     t.index ["account_type", "account_id"], name: "index_notifsettings_on_account_type_and_account_id"
     t.index ["ignore_type", "ignore_id"], name: "index_notifsettings_on_ignore_type_and_ignore_id"
+    t.index ["user_id"], name: "index_notifsettings_on_user_id"
   end
 
   create_table "pageowners", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -412,6 +414,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
   add_foreign_key "followers", "users"
   add_foreign_key "memorials", "users"
   add_foreign_key "notifications", "users"
+  add_foreign_key "notifsettings", "users"
   add_foreign_key "pageowners", "users"
   add_foreign_key "posts", "memorials"
   add_foreign_key "postslikes", "posts"
