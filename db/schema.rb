f630a4f8cb52e54a99822c72dedb326d57f7f72b
Kimson Wong <kimsonwong@gmail.com>
2021-02-01 11:59:33 +0800

    MySQL updates

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_01_18_080547) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -25,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -36,7 +33,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "alm_roles", force: :cascade do |t|
+  create_table "alm_roles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
     t.bigint "resource_id"
@@ -47,7 +44,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["resource_type", "resource_id"], name: "index_alm_roles_on_resource_type_and_resource_id"
   end
 
-  create_table "alm_users", force: :cascade do |t|
+  create_table "alm_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -89,7 +86,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["uid", "provider"], name: "index_alm_users_on_uid_and_provider", unique: true
   end
 
-  create_table "alm_users_alm_roles", id: false, force: :cascade do |t|
+  create_table "alm_users_alm_roles", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "alm_user_id"
     t.bigint "alm_role_id"
     t.index ["alm_role_id"], name: "index_alm_users_alm_roles_on_alm_role_id"
@@ -97,7 +94,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["alm_user_id"], name: "index_alm_users_alm_roles_on_alm_user_id"
   end
 
-  create_table "blms", force: :cascade do |t|
+  create_table "blms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "location"
     t.string "precinct"
     t.datetime "dob"
@@ -117,7 +114,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.boolean "hideFollowers"
   end
 
-  create_table "comments", force: :cascade do |t|
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "post_id", null: false
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
@@ -128,7 +125,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
-  create_table "commentslikes", force: :cascade do |t|
+  create_table "commentslikes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "commentable_type", null: false
     t.bigint "commentable_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -139,7 +136,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["commentable_type", "commentable_id"], name: "index_commentslikes_on_commentable_type_and_commentable_id"
   end
 
-  create_table "followers", force: :cascade do |t|
+  create_table "followers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "page_type", null: false
     t.bigint "page_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -150,7 +147,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["page_type", "page_id"], name: "index_followers_on_page_type_and_page_id"
   end
 
-  create_table "memorials", force: :cascade do |t|
+  create_table "memorials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "birthplace"
     t.datetime "dob"
     t.datetime "rip"
@@ -169,7 +166,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.boolean "hideFollowers"
   end
 
-  create_table "notifications", force: :cascade do |t|
+  create_table "notifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "read"
@@ -184,7 +181,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient_type_and_recipient_id"
   end
 
-  create_table "notifsettings", force: :cascade do |t|
+  create_table "notifsettings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "newMemorial"
@@ -199,7 +196,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["account_type", "account_id"], name: "index_notifsettings_on_account_type_and_account_id"
   end
 
-  create_table "pageowners", force: :cascade do |t|
+  create_table "pageowners", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "page_type", null: false
     t.bigint "page_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -211,7 +208,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["page_type", "page_id"], name: "index_pageowners_on_page_type_and_page_id"
   end
 
-  create_table "pg_search_documents", force: :cascade do |t|
+  create_table "pg_search_documents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.string "searchable_type"
     t.bigint "searchable_id"
@@ -220,7 +217,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "body"
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
@@ -235,7 +232,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["page_type", "page_id"], name: "index_posts_on_page_type_and_page_id"
   end
 
-  create_table "postslikes", force: :cascade do |t|
+  create_table "postslikes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -245,7 +242,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["post_id"], name: "index_postslikes_on_post_id"
   end
 
-  create_table "relationships", force: :cascade do |t|
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "page_type", null: false
     t.bigint "page_id", null: false
     t.string "relationship"
@@ -257,7 +254,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["page_type", "page_id"], name: "index_relationships_on_page_type_and_page_id"
   end
 
-  create_table "replies", force: :cascade do |t|
+  create_table "replies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "comment_id", null: false
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
@@ -268,7 +265,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["comment_id"], name: "index_replies_on_comment_id"
   end
 
-  create_table "reports", force: :cascade do |t|
+  create_table "reports", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "subject"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
@@ -278,7 +275,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["reportable_type", "reportable_id"], name: "index_reports_on_reportable_type_and_reportable_id"
   end
 
-  create_table "roles", force: :cascade do |t|
+  create_table "roles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
     t.bigint "resource_id"
@@ -289,7 +286,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
-  create_table "sessions", force: :cascade do |t|
+  create_table "sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "session_id", null: false
     t.text "data"
     t.datetime "created_at", precision: 6, null: false
@@ -298,7 +295,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
-  create_table "shares", force: :cascade do |t|
+  create_table "shares", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "content_type"
     t.integer "content_type_id"
@@ -306,7 +303,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "tagpeople", force: :cascade do |t|
+  create_table "tagpeople", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -316,7 +313,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["post_id"], name: "index_tagpeople_on_post_id"
   end
 
-  create_table "transactions", force: :cascade do |t|
+  create_table "transactions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "page_type", null: false
     t.bigint "page_id", null: false
     t.float "amount"
@@ -328,7 +325,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["page_type", "page_id"], name: "index_transactions_on_page_type_and_page_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -370,7 +367,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_080547) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
-  create_table "users_roles", id: false, force: :cascade do |t|
+  create_table "users_roles", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "role_id"
     t.index ["role_id"], name: "index_users_roles_on_role_id"
