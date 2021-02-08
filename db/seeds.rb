d1b64f72c306280d2974391a3598dcd678d275d6
Kimson Wong <kimsonwong@gmail.com>
2021-02-08 19:31:12 +0800

    Seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,11 +19,10 @@ admin = User.new(
             account_type: 1,
             phone_number: '09053536495',
             email: "admin@email.com",
-            image: File.open(Rails.root.join('app', 'assets', 'images', 'admin.jpg')),
+            image: File.open(Rails.root.join('assets', 'images', 'admin.jpg')),
             username: "admin",
             password: 'admin123',
             is_verified: true
         )
-
 admin.save 
 admin.add_role :admin
