d22c6e611e5ed713a2af09d6cd180aac6dac62e1
Kimson Wong <kimsonwong@gmail.com>
2021-02-17 11:23:39 +0800

    user updated

--- src/components/Content/UserDashboard/UserProfile/User.js
+++ src/components/Content/UserDashboard/UserProfile/User.js
@@ -35,7 +35,7 @@ export default function User() {
         console.log("Response: ", response.data);
       })
       .catch((error) => {
-        console.log(error.response);
+        console.log(error);
       });
   }, user.id);
 
