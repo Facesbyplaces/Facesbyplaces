c8c05bfd06034a38483c9f5b6219ea39b4e82304
Kimson Wong <kimsonwong@gmail.com>
2021-05-07 18:38:37 +0800

    Newly installed screens changes

--- lib/UI/ui-03-newly-installed.dart
+++ lib/UI/ui-03-newly-installed.dart
@@ -32,64 +32,54 @@ class UINewlyInstalledState extends State<UINewlyInstalled>{
       child: DefaultTabController(
         length: 3,
         child: Scaffold(
+          backgroundColor: const Color(0xffeeeeee),
           body: SingleChildScrollView(
-            physics: ClampingScrollPhysics(),
-            child: Stack(
+            physics: const ClampingScrollPhysics(),
+            child: Column(
               children: [
                 Container(
-                  height: SizeConfig.screenHeight! + 100,
+                  height: SizeConfig.screenHeight,
                   child: TabBarView(
                     children: screens,
                   ),
                 ),
 
                 Container(
-                  height: SizeConfig.screenHeight! + 50,
-                  width: SizeConfig.screenWidth,
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 50,
-                    width: 100,
-                    child: TabBar(
-                      isScrollable: false,
-                      labelColor: Color(0xff04ECFF),
-                      unselectedLabelColor: Color(0xffCDEAEC),
-                      indicatorColor: Color(0xff04ECFF),
-                      indicator: BoxDecoration(
-                        color: Colors.transparent
-                      ),
-                      indicatorSize: TabBarIndicatorSize.label,
-                      tabs: [
-
-                        Icon(Icons.circle, size: 15,),
-
-                        Icon(Icons.circle, size: 15,),
-
-                        Icon(Icons.circle, size: 15,),
-                      ],
+                  alignment: Alignment.center,
+                  height: 50,
+                  width: 100,
+                  child: TabBar(
+                    isScrollable: false,
+                    labelColor: const Color(0xff04ECFF),
+                    unselectedLabelColor: const Color(0xffCDEAEC),
+                    indicatorColor: const Color(0xff04ECFF),
+                    indicator: BoxDecoration(
+                      color: Colors.transparent
                     ),
+                    indicatorSize: TabBarIndicatorSize.label,
+                    tabs: [
+                      const Icon(Icons.circle, size: 15,),
+
+                      const Icon(Icons.circle, size: 15,),
+
+                      const Icon(Icons.circle, size: 15,),
+                    ],
                   ),
                 ),
 
-
-                Container(
-                  height: SizeConfig.screenHeight! + 100,
-                  width: SizeConfig.screenWidth,
-                  alignment: Alignment.bottomCenter,
-                  child: MaterialButton(
-                    padding: EdgeInsets.zero,
-                    child: const Text('Next', style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: const Color(0xffffffff),),),
-                    minWidth: 200,
-                    height: 45,
-                    shape: const StadiumBorder(),
-                    color: const Color(0xff04ECFF),
-                    onPressed: (){
-                      Navigator.pushReplacementNamed(context, '/start');
-                    },
-                  ),
+                MaterialButton(
+                  padding: EdgeInsets.zero,
+                  child: const Text('Next', style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: const Color(0xffffffff),),),
+                  minWidth: 200,
+                  height: 45,
+                  shape: const StadiumBorder(),
+                  color: const Color(0xff04ECFF),
+                  onPressed: (){
+                    Navigator.pushReplacementNamed(context, '/start');
+                  },
                 ),
 
+                const SizedBox(height: 20,),
               ],
             ),
           ),
@@ -108,7 +98,7 @@ class UINewlyInstalled01 extends StatelessWidget{
         children: [
           Container(
             decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.5),
+              color: const Color(0xffffffff).withOpacity(0.5),
               image: DecorationImage(
                 fit: BoxFit.cover,
                 image: const AssetImage('assets/icons/stephen-hawking.jpeg'),
@@ -117,7 +107,7 @@ class UINewlyInstalled01 extends StatelessWidget{
             child: new BackdropFilter(
               filter: new ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
               child: new Container(
-                decoration: new BoxDecoration(color: Colors.white.withOpacity(0.0)),
+                decoration: new BoxDecoration(color: const Color(0xffffffff).withOpacity(0.0)),
               ),
             ),
           ),
@@ -140,8 +130,8 @@ class UINewlyInstalled01 extends StatelessWidget{
 
                   const SizedBox(height: 20,),
 
-                  Text('Stephen Hawking',
-                    style: TextStyle(
+                  const Text('Stephen Hawking',
+                    style: const TextStyle(
                       fontSize: 24,
                       color: const Color(0xff04ECFF),
                     )
@@ -152,20 +142,18 @@ class UINewlyInstalled01 extends StatelessWidget{
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Icon(MdiIcons.starOutline, color: Color(0xffffffff),),
+                      const Icon(MdiIcons.starOutline, color: const Color(0xffffffff),),
                       
                       const SizedBox(width: 5,),
 
                       RichText(
-                        text: TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'Born ',
-                            ),
+                        text: const TextSpan(
+                          children: const <TextSpan>[
+                            const TextSpan(text: 'Born ',),
 
-                            TextSpan(
+                            const TextSpan(
                               text: 'January 8, 1942',
-                              style: TextStyle(
+                              style: const TextStyle(
                                 fontWeight: FontWeight.bold
                               )
                             ),
@@ -180,20 +168,18 @@ class UINewlyInstalled01 extends StatelessWidget{
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Icon(MdiIcons.graveStone, color: Color(0xffffffff),),
+                      const Icon(MdiIcons.graveStone, color: const Color(0xffffffff),),
                       
                       const SizedBox(width: 5,),
 
                       RichText(
-                        text: TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'Died ',
-                            ),
+                        text: const TextSpan(
+                          children: const <TextSpan>[
+                            const TextSpan(text: 'Died ',),
 
-                            TextSpan(
+                            const TextSpan(
                               text: 'March 14, 2018',
-                              style: TextStyle(
+                              style: const TextStyle(
                                 fontWeight: FontWeight.bold
                               )
                             ),
@@ -205,8 +191,7 @@ class UINewlyInstalled01 extends StatelessWidget{
 
                   const SizedBox(height: 10,),
 
-                  const Text('"If human life were long enough to find the ultimate theory, everything would have been solved by previous generations. Nothing would be left to be discovered."', textAlign: TextAlign.center, style: TextStyle(color: Color(0xffffffff),),),
-
+                  const Text('"If human life were long enough to find the ultimate theory, everything would have been solved by previous generations. Nothing would be left to be discovered."', textAlign: TextAlign.center, style: const TextStyle(color: const Color(0xffffffff),),),
                 ],
               ),
             ),
@@ -235,7 +220,7 @@ class UINewlyInstalled02 extends StatelessWidget{
             child: new BackdropFilter(
               filter: new ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
               child: new Container(
-                decoration: new BoxDecoration(color: Colors.white.withOpacity(0.0)),
+                decoration: new BoxDecoration(color: const Color(0xffffffff).withOpacity(0.0)),
               ),
             ),
           ),
@@ -258,8 +243,8 @@ class UINewlyInstalled02 extends StatelessWidget{
 
                   const SizedBox(height: 20,),
 
-                  Text('Kobe Bryant',
-                    style: TextStyle(
+                  const Text('Kobe Bryant',
+                    style: const TextStyle(
                       fontSize: 24,
                       color: const Color(0xff04ECFF),
                     )
@@ -270,20 +255,18 @@ class UINewlyInstalled02 extends StatelessWidget{
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Icon(MdiIcons.starOutline, color: Color(0xffffffff),),
+                      const Icon(MdiIcons.starOutline, color: const Color(0xffffffff),),
                       
                       const SizedBox(width: 5,),
 
                       RichText(
-                        text: TextSpan(
+                        text: const TextSpan(
                           children: <TextSpan>[
-                            TextSpan(
-                              text: 'Born ',
-                            ),
+                            const TextSpan(text: 'Born ',),
 
-                            TextSpan(
+                            const TextSpan(
                               text: 'August 23, 1978',
-                              style: TextStyle(
+                              style: const TextStyle(
                                 fontWeight: FontWeight.bold
                               )
                             ),
@@ -298,20 +281,18 @@ class UINewlyInstalled02 extends StatelessWidget{
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Icon(MdiIcons.graveStone, color: Color(0xffffffff),),
+                      const Icon(MdiIcons.graveStone, color: const Color(0xffffffff),),
                       
                       const SizedBox(width: 5,),
 
                       RichText(
-                        text: TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'Died ',
-                            ),
+                        text: const TextSpan(
+                          children: const <TextSpan>[
+                            const TextSpan(text: 'Died ',),
 
-                            TextSpan(
+                            const TextSpan(
                               text: 'January 26, 2020',
-                              style: TextStyle(
+                              style: const TextStyle(
                                 fontWeight: FontWeight.bold
                               )
                             ),
@@ -323,8 +304,7 @@ class UINewlyInstalled02 extends StatelessWidget{
 
                   const SizedBox(height: 10,),
 
-                  const Text('"I\'ll do whatever it takes to win games, whether it\'s sitting on a bench waving a towel, handing a cup of water to a teammate, or hitting the game-winning shot."', textAlign: TextAlign.center, style: TextStyle(color: Color(0xffffffff),),),
-
+                  const Text('"I\'ll do whatever it takes to win games, whether it\'s sitting on a bench waving a towel, handing a cup of water to a teammate, or hitting the game-winning shot."', textAlign: TextAlign.center, style: const TextStyle(color: const Color(0xffffffff),),),
                 ],
               ),
             ),
@@ -344,8 +324,8 @@ class UINewlyInstalled03 extends StatelessWidget{
         children: [
           Container(
             decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.5),
-              image: DecorationImage(
+              color: const Color(0xffffffff).withOpacity(0.5),
+              image: const DecorationImage(
                 fit: BoxFit.cover,
                 image: const AssetImage('assets/icons/stan-lee.jpeg'),
               ),
@@ -353,7 +333,7 @@ class UINewlyInstalled03 extends StatelessWidget{
             child: new BackdropFilter(
               filter: new ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
               child: new Container(
-                decoration: new BoxDecoration(color: Colors.white.withOpacity(0.0)),
+                decoration: new BoxDecoration(color: const Color(0xffffffff).withOpacity(0.0)),
               ),
             ),
           ),
@@ -376,8 +356,8 @@ class UINewlyInstalled03 extends StatelessWidget{
 
                   const SizedBox(height: 20,),
 
-                  Text('Stan Lee',
-                    style: TextStyle(
+                  const Text('Stan Lee',
+                    style: const TextStyle(
                       fontSize: 24,
                       color: const Color(0xff04ECFF),
                     )
@@ -388,20 +368,18 @@ class UINewlyInstalled03 extends StatelessWidget{
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Icon(MdiIcons.starOutline, color: Color(0xffffffff),),
+                      const Icon(MdiIcons.starOutline, color: const Color(0xffffffff),),
                       
                       const SizedBox(width: 5,),
 
                       RichText(
-                        text: TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'Born ',
-                            ),
+                        text: const TextSpan(
+                          children: const <TextSpan>[
+                            const TextSpan(text: 'Born ',),
 
-                            TextSpan(
+                            const TextSpan(
                               text: 'December 28, 1922',
-                              style: TextStyle(
+                              style: const TextStyle(
                                 fontWeight: FontWeight.bold
                               )
                             ),
@@ -416,20 +394,18 @@ class UINewlyInstalled03 extends StatelessWidget{
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Icon(MdiIcons.graveStone, color: Color(0xffffffff),),
+                      const Icon(MdiIcons.graveStone, color: const Color(0xffffffff),),
                       
                       const SizedBox(width: 5,),
 
                       RichText(
-                        text: TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'Died ',
-                            ),
+                        text: const TextSpan(
+                          children: const <TextSpan>[
+                            const TextSpan(text: 'Died ',),
 
-                            TextSpan(
+                            const TextSpan(
                               text: 'November 12, 2018',
-                              style: TextStyle(
+                              style: const TextStyle(
                                 fontWeight: FontWeight.bold
                               )
                             ),
@@ -441,8 +417,7 @@ class UINewlyInstalled03 extends StatelessWidget{
 
                   const SizedBox(height: 10,),
 
-                  const Text('"I\'ve been the luckiest man in the world because I\'ve had friends, and to have the right friends is everything: people you can depend on, people who tell you the truth if you ask something."', textAlign: TextAlign.center, style: TextStyle(color: Color(0xffffffff),),),
-
+                  const Text('"I\'ve been the luckiest man in the world because I\'ve had friends, and to have the right friends is everything: people you can depend on, people who tell you the truth if you ask something."', textAlign: TextAlign.center, style: const TextStyle(color: const Color(0xffffffff),),),
                 ],
               ),
             ),
