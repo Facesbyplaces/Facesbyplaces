f572361e443fa085cd1faa9e7961adfba6449c50
Kimson Wong <kimsonwong@gmail.com>
2021-06-30 15:51:24 +0800

    migration updated

--- db/migrate/20210629103333_add_customer_account_to_users.rb
+++ db/migrate/20210629103333_add_customer_account_to_users.rb
@@ -1,7 +1,6 @@
 class AddCustomerAccountToUsers < ActiveRecord::Migration[6.0]
   def change
-    remove_column :users, :platform_account_customer, :string
-    remove_column :users, :connected_account_customer, :string
+    remove_column :users, :stripe_customer_account, :string
     add_column :users, :platform_account_customer, :string
     add_column :users, :connected_account_customer, :string
   end
--- db/migrate/20210629103352_add_customer_account_to_alm_users.rb
+++ db/migrate/20210629103352_add_customer_account_to_alm_users.rb
@@ -1,5 +1,6 @@
 class AddCustomerAccountToAlmUsers < ActiveRecord::Migration[6.0]
   def change
+    remove_column :alm_users, :stripe_customer_account, :string
     add_column :alm_users, :platform_account_customer, :string
     add_column :alm_users, :connected_account_customer, :string
   end
