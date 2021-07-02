424565f1e16a5e7e3438c0e90761137622eaced1
Kimson Wong <kimsonwong@gmail.com>
2021-07-02 16:12:37 +0800

    set payment intent for apple pay test updated

--- app/controllers/api/v1/payments/payment_intent_controller.rb
+++ app/controllers/api/v1/payments/payment_intent_controller.rb
@@ -36,7 +36,7 @@ class Api::V1::Payments::PaymentIntentController < ApplicationController
         }, status: 404
       end
     elsif intent.status == 'requires_payment_method'
-      render json: { client_secret: intent.client_secret, token: token }, status: 200
+      render json: { client_secret: intent.client_secret, payment_intent: intent.id, token: token }, status: 200
     end
   end
 
