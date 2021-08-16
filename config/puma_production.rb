aeba4b3e76407adce135c2d389a602cee5cd2dba
Kimson Wong <kimsonwong@gmail.com>
2021-08-16 09:48:22 +0800

    configuration updated

--- config/puma_production.rb
+++ config/puma_production.rb
@@ -5,9 +5,11 @@
 # and maximum; this matches the default thread size of Active Record.
 #
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+daemonize true
+
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
+
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -17,23 +19,21 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "production" }
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
 # Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
+# Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers 1
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-preload_app!
+# preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
