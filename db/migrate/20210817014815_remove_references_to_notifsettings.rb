736f7dec00608dc7a87dfdb02d3ecef1a98ee2f3
Kimson Wong <kimsonwong@gmail.com>
2021-08-17 10:27:08 +0800

    remove references to notifsettings migration updated

--- db/migrate/20210817014815_remove_references_to_notifsettings.rb
+++ db/migrate/20210817014815_remove_references_to_notifsettings.rb
@@ -1,6 +1,6 @@
 class RemoveReferencesToNotifsettings < ActiveRecord::Migration[6.0]
   def change
-    remove_reference :notifsettings, :ignore, polymorphic: true, null: false if Notifsetting.column_names.include?('ignore')
-    remove_reference :notifsettings, :user, null: false, foreign_key: true if Notifsetting.column_names.include?('user')
+    remove_reference :notifsettings, :ignore, polymorphic: true, null: false if Notifsetting.column_names.include?('ignore_id')
+    remove_reference :notifsettings, :user, null: false, foreign_key: true if Notifsetting.column_names.include?('user_id')
   end
 end
