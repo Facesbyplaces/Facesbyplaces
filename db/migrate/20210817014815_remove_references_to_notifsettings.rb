24808f4b3750bb32888954e42a53d1120af6ecd4
Kimson Wong <kimsonwong@gmail.com>
2021-08-18 10:55:39 +0800

    removed notify and user reference from notifications table

--- db/migrate/20210817014815_remove_references_to_notifsettings.rb
+++ db/migrate/20210817014815_remove_references_to_notifsettings.rb
@@ -6,7 +6,7 @@ class RemoveReferencesToNotifsettings < ActiveRecord::Migration[6.0]
     remove_column :blms, :user_id, :integer if Blm.column_names.include?('user_id')
     remove_column :pageowners, :user_id, :integer if Pageowner.column_names.include?('user_id')
     remove_column :relationships, :user_id, :integer if Relationship.column_names.include?('user_id')
-    remove_reference :notifications, :notify, polymorphic: true, null: false if Notification.column_names.include?('notif_id')
+    remove_reference :notifications, :notify, polymorphic: true, null: false if Notification.column_names.include?('notify_id')
     remove_reference :notifications, :user, null: false, foreign_key: true if Notification.column_names.include?('user_id')
   end
 end
