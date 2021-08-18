a5b52b344dbf5e431e3704dfd2b0f9ce1953c622
Kimson Wong <kimsonwong@gmail.com>
2021-08-18 17:44:09 +0800

    removed memorial_id reference from posts table

--- db/migrate/20210817014815_remove_references_to_notifsettings.rb
+++ db/migrate/20210817014815_remove_references_to_notifsettings.rb
@@ -8,5 +8,6 @@ class RemoveReferencesToNotifsettings < ActiveRecord::Migration[6.0]
     remove_column :relationships, :user_id, :integer if Relationship.column_names.include?('user_id')
     remove_reference :notifications, :notify, polymorphic: true, null: false if Notification.column_names.include?('notify_id')
     remove_reference :notifications, :user, null: false, foreign_key: true if Notification.column_names.include?('user_id')
+    remove_column :posts, :memorial_id, :integer if Post.column_names.include?('memorial_id')
   end
 end
