fc00147586af773cacb535a013c9138724444810
Kimson Wong <kimsonwong@gmail.com>
2021-08-24 16:31:20 +0800

    hamburger menu added

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -1,19 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $body = $("body");
   $headerSub = $body.find(".header-sub");
-  $headerSub.find(".logo-container .logo").on("click", function(){
-    if($headerSub.find(".tabs-container").hasClass("opened")) {
+  $headerSub.find(".logo-container .logo").on("click", function () {
+    if ($headerSub.find(".tabs-container").hasClass("opened")) {
       $headerSub.hide("slide", { direction: "left" }, 500);
-      setTimeout(
-      function() 
-      {
+      setTimeout(function () {
         $body.find(".tabs-container").removeClass("opened").hide();
         $headerSub.show();
       }, 600);
-    }
-    else {
-      console.log("something")
-      $headerSub.find(".tabs-container").show("slide", { direction: "left" }, 500);
+    } else {
+      console.log("something");
+      $headerSub
+        .find(".tabs-container")
+        .show("slide", { direction: "left" }, 500);
       $headerSub.find(".tabs-container").addClass("opened");
     }
   });
