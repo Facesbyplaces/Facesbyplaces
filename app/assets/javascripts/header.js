e3e4bd58f3229faca91eed22930d996201743e7a
Kimson Wong <kimsonwong@gmail.com>
2021-08-26 16:09:05 +0800

    hamburger menu updated

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,19 +1,22 @@
 $(document).ready(function () {
   $body = $("body");
+  $headerSubTab = $body.find(".header-sub-tab");
   $headerSub = $body.find(".header-sub");
   $headerSub.find(".logo-container .logo").on("click", function () {
-    if ($headerSub.find(".tabs-container").hasClass("opened")) {
-      $headerSub.hide("slide", { direction: "right" }, 500);
+    if ($headerSubTab.find(".tabs-container").hasClass("opened")) {
+      $headerSubTab
+        .find(".tabs-container")
+        .hide("slide", { direction: "up" }, 500);
       setTimeout(function () {
         $body.find(".tabs-container").removeClass("opened").hide();
-        $headerSub.show();
+        $headerSubTab.show();
       }, 600);
     } else {
       console.log("something");
-      $headerSub
+      $headerSubTab
         .find(".tabs-container")
-        .show("slide", { direction: "left" }, 500);
-      $headerSub.find(".tabs-container").addClass("opened");
+        .show("slide", { direction: "up" }, 500);
+      $headerSubTab.find(".tabs-container").addClass("opened");
     }
   });
 });
