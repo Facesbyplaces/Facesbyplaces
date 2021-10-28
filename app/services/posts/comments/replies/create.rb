e816a291c6fa4c3255f63e71a66ffd12b500bdd0
Kimson Wong <kimsonwong@gmail.com>
2021-10-28 15:36:10 +0800

    resolved reply notif issue

--- app/services/posts/comments/replies/create.rb
+++ app/services/posts/comments/replies/create.rb
@@ -1,8 +1,8 @@
 class Posts::Comments::Replies::Create
 
     def initialize( reply:, user: )
-        @reply                  = reply
-        @user                   = user
+        @reply = reply
+        @user  = user
     end
 
     def execute
@@ -12,7 +12,7 @@ class Posts::Comments::Replies::Create
         if Comment.find(reply.comment_id)
             if reply.save
                 # Add to notification
-                notify_followers_of_a_reply(reply)
+                notify_commentor_of_a_reply(reply)
                 return true
             else
                 return reply.errors
@@ -22,68 +22,81 @@ class Posts::Comments::Replies::Create
 
     private
 
-    def notify_followers_of_a_reply(reply)
-        if reply.comment.replies.count == 1
-            if @user != reply.comment.account
-                if reply.comment.account.notifsetting.postComments == true
-                    Notification::Builder.new(
-                        device_tokens:  reply.comment.account.device_token,
-                        title:          "FacesByPlaces Notification",
-                        message:        "#{@user.first_name} replied to your comment",
-                        recipient:      reply.comment.account,
-                        actor:          @user,
-                        data:           reply.comment.post.id,
-                        type:           "Post",
-                        postType:       reply.comment.post.page_type,
-                    ).notify
-                end
-            end
-        else
-            users = reply.comment.accounts.uniq - [@user]
-            if users.count == 0
-                if reply.comment.account.notifsetting.postComments == true
-                    Notification::Builder.new(
-                        device_tokens:  reply.comment.account.device_token,
-                        title:          "FacesByPlaces Notification",
-                        message:        "#{@user.first_name} replied to your comment",
-                        recipient:      reply.comment.account,
-                        actor:          @user,
-                        data:           reply.comment.post.id,
-                        type:           "Post",
-                        postType:       reply.comment.post.page_type,
-                    ).notify
-                end
-            else
-                users.each do |user|
-                    if user.notifsetting.postComments == true
-                        if reply.comment.account == user
-                            Notification::Builder.new(
-                                device_tokens:  reply.comment.account.device_token,
-                                title:          "FacesByPlaces Notification",
-                                message:        "#{@user.first_name} replied to your comment",
-                                recipient:      reply.comment.account,
-                                actor:          @user,
-                                data:           reply.comment.post.id,
-                                type:           "Post",
-                                postType:       reply.comment.post.page_type,
-                            ).notify
-                        else
-                            Notification::Builder.new(
-                                device_tokens:  reply.comment.account.device_token,
-                                title:          "FacesByPlaces Notification",
-                                message:        "#{@user.first_name} replied to your comment",
-                                recipient:      reply.comment.account,
-                                actor:          @user,
-                                data:           reply.comment.post.id,
-                                type:           "Post",
-                                postType:       reply.comment.post.page_type,
-                            ).notify
-                        end
-                    end
-                end
-                
-            end
+    def notify_commentor_of_a_reply(reply)
+        if reply.comment.account.notifsetting.postComments == true
+            Notification::Builder.new(
+                device_tokens:  reply.comment.account.device_token,
+                title:          "FacesByPlaces Notification",
+                message:        "#{@user.first_name} replied to your comment",
+                recipient:      reply.comment.account,
+                actor:          @user,
+                data:           reply.comment.post.id,
+                type:           "Post",
+                postType:       reply.comment.post.page_type,
+            ).notify
         end
+
+        # if reply.comment.replies.count == 1
+        #     if @user != reply.comment.account
+        #         if reply.comment.account.notifsetting.postComments == true
+        #             Notification::Builder.new(
+        #                 device_tokens:  reply.comment.account.device_token,
+        #                 title:          "FacesByPlaces Notification",
+        #                 message:        "#{@user.first_name} replied to your comment",
+        #                 recipient:      reply.comment.account,
+        #                 actor:          @user,
+        #                 data:           reply.comment.post.id,
+        #                 type:           "Post",
+        #                 postType:       reply.comment.post.page_type,
+        #             ).notify
+        #         end
+        #     end
+        # else
+        #     users = reply.comment.accounts.uniq - [@user]
+        #     if users.count == 0
+        #         if reply.comment.account.notifsetting.postComments == true
+        #             Notification::Builder.new(
+        #                 device_tokens:  reply.comment.account.device_token,
+        #                 title:          "FacesByPlaces Notification",
+        #                 message:        "#{@user.first_name} replied to your comment",
+        #                 recipient:      reply.comment.account,
+        #                 actor:          @user,
+        #                 data:           reply.comment.post.id,
+        #                 type:           "Post",
+        #                 postType:       reply.comment.post.page_type,
+        #             ).notify
+        #         end
+        #     else
+        #         users.each do |user|
+        #             if user.notifsetting.postComments == true
+        #                 if reply.comment.account == user
+        #                     Notification::Builder.new(
+        #                         device_tokens:  reply.comment.account.device_token,
+        #                         title:          "FacesByPlaces Notification",
+        #                         message:        "#{@user.first_name} replied to your comment",
+        #                         recipient:      reply.comment.account,
+        #                         actor:          @user,
+        #                         data:           reply.comment.post.id,
+        #                         type:           "Post",
+        #                         postType:       reply.comment.post.page_type,
+        #                     ).notify
+        #                 else
+        #                     Notification::Builder.new(
+        #                         device_tokens:  reply.comment.account.device_token,
+        #                         title:          "FacesByPlaces Notification",
+        #                         message:        "#{@user.first_name} replied to your comment",
+        #                         recipient:      reply.comment.account,
+        #                         actor:          @user,
+        #                         data:           reply.comment.post.id,
+        #                         type:           "Post",
+        #                         postType:       reply.comment.post.page_type,
+        #                     ).notify
+        #                 end
+        #             end
+        #         end
+                
+        #     end
+        # end
     end
     
 end
