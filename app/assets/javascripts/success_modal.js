4f06ad8f027b123dc3635baa68770e2f177e6850
Kimson Wong <kimsonwong@gmail.com>
2021-11-04 19:44:27 +0800

    added success modal after newsletter submission

--- app/assets/javascripts/success_modal.js
+++ app/assets/javascripts/success_modal.js
@@ -11,11 +11,13 @@ $(document).ready(function () {
   // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
     modal.style.display = "none";
+    location.reload();
   };
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function (event) {
     if (event.target == modal) {
+      location.reload();
       modal.style.display = "none";
     }
   };
