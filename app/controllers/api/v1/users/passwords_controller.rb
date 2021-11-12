8a598095413103f3b77aff27baf88cd27a372858
Kimson Wong <kimsonwong@gmail.com>
2021-11-12 13:31:34 +0800

    error display updated

--- app/controllers/api/v1/users/passwords_controller.rb
+++ app/controllers/api/v1/users/passwords_controller.rb
@@ -16,4 +16,16 @@ class Api::V1::Users::PasswordsController < DeviseTokenAuth::PasswordsController
       }, scope: :current_master
     end
 
+    def render_not_found_error
+      if Devise.paranoid
+        render_error(200, I18n.t('devise_token_auth.passwords.sended_paranoid'))
+      else
+        render_error(200, I18n.t('devise_token_auth.passwords.user_not_found', email: @email))
+      end
+    end
+
+    def render_error(status, message, data = nil)
+      render json: { success: false, errors: message, status: status }
+    end
+
 end
