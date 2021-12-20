4f3da12262d86845ace6642493ff18e0cf8c4b33
Kimson Wong <kimsonwong@gmail.com>
2021-12-20 11:19:42 +0800

    hidden user details issue update

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -10,4 +10,44 @@ class UserSerializer < ActiveModel::Serializer
       return ""
     end
   end
+
+  def birthdate
+    if object.hideBirthdate 
+      return ""
+    else
+      object.birthdate
+    end
+  end
+
+  def birthplace
+    if object.hideBirthplace
+      return ""
+    else
+      object.birthplace
+    end
+  end
+
+  def email
+    if object.hideEmail
+      return ""
+    else
+      object.email
+    end
+  end
+
+  def address
+    if object.hideAddress
+      return ""
+    else
+      object.address
+    end
+  end
+
+  def phone_number
+    if object.hidePhonenumber
+      return ""
+    else
+      object.phone_number
+    end
+  end
 end
